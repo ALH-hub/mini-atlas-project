7dd11428af79b638b512c16e014015e5d8b1b154
Alhadji Oumate <alhadjioum0@gmail.com>
2024-07-10 15:56:15 +0100

    interface of notes upload page

--- client/src/components/teacher/Write.jsx
+++ client/src/components/teacher/Write.jsx
@@ -3,10 +3,10 @@ import 'react-quill/dist/quill.snow.css';
 
 const Write = (value) => {
   return (
-    <div className='pt-20 h-screen'>
+    <div className='flex flex-grow overflow-auto justify-center pt-40'>
       <form
         action=''
-        className='flex flex-col gap-6 items-center h-5/6 border border-black p-4 w-2/3 mx-auto rounded-md shadow-md'
+        className='flex flex-col gap-6 items-center h-5/6 border border-gray-300 p-4 w-[65rem] mx-auto rounded-md shadow-md'
       >
         <input
           className='w-1/2 p-2 border border-gray-300 rounded-md shadow-sm'
@@ -16,16 +16,18 @@ const Write = (value) => {
           id='title'
         />
         <ReactQuill
-          className='w-full p-2 rounded-md h-4/6 mb-4'
+          className='w-full p-2 rounded-md mb-4 h-4/5'
           theme='snow'
           value={value ? value : ''}
           placeholder='Enter Chapter content here'
           id='content'
           name='content'
         />
-        <div className='flex justify-between gap-8 mt-4'>
-          <button className='bg-blue-300 p-2 rounded-md shadow-md'>Save</button>
-          <button className='bg-blue-300 p-2 rounded-md shadow-md'>
+        <div className='flex justify-end w-full gap-8 mt-4'>
+          <button className='border border-lightgray-400 px-2 py-1 text-sm rounded h-fit hover:bg-blue-200'>
+            Save
+          </button>
+          <button className='border border-lightgray-400 px-2 py-1 text-sm rounded h-fit hover:bg-blue-200'>
             Cancel
           </button>
         </div>
