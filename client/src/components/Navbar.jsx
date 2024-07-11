9608e6c8986c6a74a9c4008b01f1ce30dc8b7508
Alhadji Oumate <alhadjioum0@gmail.com>
2024-07-11 14:22:24 +0100

    refactor: restructure the general top navbarg

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,10 +15,6 @@ const Navbar = () => {
   return (
     <nav className='flex justify-between py-2 px-10 border border-gray-400 z-10 bg-white fixed inset-x-0'>
       <img className='rounded-xl' width='40px' src={logo} alt='' />
-      <div className='flex items-center gap-4'>
-        <img className='rounded-xl' src={csc} alt='' width='40px' />
-        <span className='text-sm'>Computer Sc.</span>
-      </div>
 
       <div className='flex items-center gap-4'>
         <img className='rounded-xl' src={user} alt='' width='40px' />
