01861093adc5c440051b4ff2baced0734818b79c
Alhadji Oumate <alhadjioum0@gmail.com>
2024-07-13 06:49:13 +0100

    feat: unauthorized page

--- client/src/components/Unauthorized.jsx
+++ client/src/components/Unauthorized.jsx
@@ -1,5 +1,15 @@
+import unauthorized from '/401.png';
+
 const Unauthorized = () => {
-  return <div>Erorr get out rapidly from here</div>;
+  return (
+    <div className='flex flex-col gap-2 justify-center w-full items-center h-screen'>
+      <img className='w-2/3' src={unauthorized} alt='401 Unauthorized' />
+      <h1 className='text-red-700 mt-4 text-6xl font-bold'>
+        Error 401: Unauthorized
+      </h1>
+      <p className='text-2xl'>You are not authorized to access this page.</p>
+    </div>
+  );
 };
 
 export default Unauthorized;
