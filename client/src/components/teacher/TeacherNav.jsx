465c52211932cb19219750d4aebb54f1f6c3c0ea
Alhadji Oumate <alhadjioum0@gmail.com>
2024-07-16 16:55:32 +0100

    feat: creation of user info on teacher nav bar on click

--- client/src/components/teacher/TeacherNav.jsx
+++ client/src/components/teacher/TeacherNav.jsx
@@ -1,10 +1,15 @@
 import user from '/user.png';
 import logo from '/logo.png';
 import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const TeacherNav = () => {
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
   const role = localStorage.getItem('role');
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   const handleLogout = () => {
     // clear browser local storage
     localStorage.clear();
@@ -23,7 +28,41 @@ const TeacherNav = () => {
           <Link to='/teacher/quiz'>Quiz</Link>
         </div>
         <div className='flex gap-4 items-center'>
-          <img className='rounded-xl' src={user} alt='' width='40px' />
+          <button onClick={toggleMenu} className='user-logo-btn'>
+            <img
+              className='rouded-xl'
+              src={user}
+              alt='User Logo'
+              width='40px'
+            />{' '}
+          </button>
+          {isOpen && (
+            <div className='absolute top-full right-4 border border-gray w-96 bg-white p-6 flex flex-col gap-2 items-center'>
+              <img
+                className='rouded-xl'
+                src={user}
+                alt='User Logo'
+                width='30px'
+              />
+              <div className='flex flex-col gap-2 m-2'>
+                <p>
+                  <b>Username:</b> {localStorage.getItem('name')}
+                </p>
+                <p>
+                  <b>Email:</b> {localStorage.getItem('email')}
+                </p>
+                <p>
+                  <b>Role:</b> {localStorage.getItem('role')}
+                </p>
+              </div>
+              <button
+                className='border border-lightgray-400 p-1 text-sm rounded h-fit hover:bg-blue-200'
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </div>
+          )}
           <button
             className='border border-lightgray-400 px-2 text-sm rounded h-fit hover:bg-blue-200'
             onClick={handleLogout}
